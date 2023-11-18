b7db5427ae72901942c94b4460a39417a12e7b5c
seyedmohammadreza <59357443+mrakharatha@users.noreply.github.com>
2023-11-18 14:16:31 +0330

    Add project files.

---
+++ ZK_RFID/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace ZK_RFID
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gpb_tcp = new System.Windows.Forms.GroupBox();
+            this.ipServerIP = new System.Windows.Forms.TextBox();
+            this.tb_Port = new System.Windows.Forms.TextBox();
+            this.btDisConnectTcp = new System.Windows.Forms.Button();
+            this.btConnectTcp = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btStartMactive = new System.Windows.Forms.Button();
+            this.timer_RealTime = new System.Windows.Forms.Timer(this.components);
+            this.ListBox = new System.Windows.Forms.ListBox();
+            this.gpb_tcp.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gpb_tcp
+            // 
+            this.gpb_tcp.Controls.Add(this.ipServerIP);
+            this.gpb_tcp.Controls.Add(this.tb_Port);
+            this.gpb_tcp.Controls.Add(this.btDisConnectTcp);
+            this.gpb_tcp.Controls.Add(this.btConnectTcp);
+            this.gpb_tcp.Controls.Add(this.label2);
+            this.gpb_tcp.Controls.Add(this.label4);
+            this.gpb_tcp.Location = new System.Drawing.Point(12, 12);
+            this.gpb_tcp.Name = "gpb_tcp";
+            this.gpb_tcp.Size = new System.Drawing.Size(343, 77);
+            this.gpb_tcp.TabIndex = 5;
+            this.gpb_tcp.TabStop = false;
+            this.gpb_tcp.Text = "TCP/IP";
+            // 
+            // ipServerIP
+            // 
+            this.ipServerIP.Location = new System.Drawing.Point(96, 19);
+            this.ipServerIP.Name = "ipServerIP";
+            this.ipServerIP.Size = new System.Drawing.Size(121, 20);
+            this.ipServerIP.TabIndex = 23;
+            this.ipServerIP.Text = "192.168.0.250";
+            // 
+            // tb_Port
+            // 
+            this.tb_Port.Location = new System.Drawing.Point(96, 43);
+            this.tb_Port.Name = "tb_Port";
+            this.tb_Port.Size = new System.Drawing.Size(121, 20);
+            this.tb_Port.TabIndex = 22;
+            this.tb_Port.Text = "27011";
+            // 
+            // btDisConnectTcp
+            // 
+            this.btDisConnectTcp.Enabled = false;
+            this.btDisConnectTcp.Location = new System.Drawing.Point(234, 43);
+            this.btDisConnectTcp.Name = "btDisConnectTcp";
+            this.btDisConnectTcp.Size = new System.Drawing.Size(90, 25);
+            this.btDisConnectTcp.TabIndex = 20;
+            this.btDisConnectTcp.Text = "Disconnect";
+            this.btDisConnectTcp.UseVisualStyleBackColor = true;
+            this.btDisConnectTcp.Click += new System.EventHandler(this.btDisConnectTcp_Click);
+            // 
+            // btConnectTcp
+            // 
+            this.btConnectTcp.Location = new System.Drawing.Point(234, 14);
+            this.btConnectTcp.Name = "btConnectTcp";
+            this.btConnectTcp.Size = new System.Drawing.Size(90, 25);
+            this.btConnectTcp.TabIndex = 19;
+            this.btConnectTcp.Text = "Connect";
+            this.btConnectTcp.UseVisualStyleBackColor = true;
+            this.btConnectTcp.Click += new System.EventHandler(this.btConnectTcp_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Port:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(51, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "IP:";
+            // 
+            // btStartMactive
+            // 
+            this.btStartMactive.Enabled = false;
+            this.btStartMactive.Font = new System.Drawing.Font("SimSun", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btStartMactive.ForeColor = System.Drawing.Color.DarkBlue;
+            this.btStartMactive.Location = new System.Drawing.Point(12, 95);
+            this.btStartMactive.Name = "btStartMactive";
+            this.btStartMactive.Size = new System.Drawing.Size(343, 43);
+            this.btStartMactive.TabIndex = 6;
+            this.btStartMactive.Text = "Start";
+            this.btStartMactive.UseVisualStyleBackColor = true;
+            this.btStartMactive.Click += new System.EventHandler(this.btStartMactive_Click);
+            // 
+            // timer_RealTime
+            // 
+            this.timer_RealTime.Interval = 50;
+            this.timer_RealTime.Tick += new System.EventHandler(this.timer_RealTime_Tick);
+            // 
+            // ListBox
+            // 
+            this.ListBox.FormattingEnabled = true;
+            this.ListBox.Location = new System.Drawing.Point(12, 144);
+            this.ListBox.Name = "ListBox";
+            this.ListBox.Size = new System.Drawing.Size(671, 472);
+            this.ListBox.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(695, 626);
+            this.Controls.Add(this.ListBox);
+            this.Controls.Add(this.btStartMactive);
+            this.Controls.Add(this.gpb_tcp);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.gpb_tcp.ResumeLayout(false);
+            this.gpb_tcp.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gpb_tcp;
+        private System.Windows.Forms.TextBox tb_Port;
+        private System.Windows.Forms.Button btDisConnectTcp;
+        private System.Windows.Forms.Button btConnectTcp;
+        private System.Windows.Forms.Label label2;
+        internal System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox ipServerIP;
+        private System.Windows.Forms.Button btStartMactive;
+        private System.Windows.Forms.Timer timer_RealTime;
+        private System.Windows.Forms.ListBox ListBox;
+    }
+}
+
---
+++ ZK_RFID/Form1.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using UHF;
+using static System.Collections.Specialized.BitVector32;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+
+namespace ZK_RFID
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private byte fComAdr = 0xff;
+        private int fCmdRet = 30;
+        private int frmcomportindex;
+        private string fInventory_EPC_List;
+        private volatile bool fIsInventoryScan = false;
+
+        private void btConnectTcp_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string ipAddress = ipServerIP.Text;
+                int nPort = Convert.ToInt32(tb_Port.Text);
+                fComAdr = 255;
+                int frmPortIndex = 0;
+                fCmdRet = RWDev.OpenNetPort(nPort, ipAddress, ref fComAdr, ref frmPortIndex);
+                if (fCmdRet != 0)
+                {
+                    string strLog = "Connect reader failed: " + GetReturnCodeDesc(fCmdRet);
+                    MessageBox.Show(strLog, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                    return;
+                }
+                else
+                {
+                    frmcomportindex = frmPortIndex;
+                    //string strLog = "Connect: " + ipAddress + "@" + nPort.ToString();
+                    //MessageBox.Show(strLog, "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                btConnectTcp.Enabled = false;
+                btDisConnectTcp.Enabled = true;
+                btStartMactive.Enabled = true;
+                btConnectTcp.ForeColor = Color.Black;
+                btDisConnectTcp.ForeColor = Color.Indigo;
+                btStartMactive.ForeColor = Color.Indigo;
+
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+        private void btDisConnectTcp_Click(object sender, EventArgs e)
+        {
+            if (frmcomportindex > 1023)
+                fCmdRet = RWDev.CloseNetPort(frmcomportindex);
+            if (fCmdRet == 0) frmcomportindex = -1;
+            btConnectTcp.Enabled = true;
+            btDisConnectTcp.Enabled = false;
+            btStartMactive.Enabled = false;
+            btConnectTcp.ForeColor = Color.Indigo;
+            btDisConnectTcp.ForeColor = Color.Black;
+            btStartMactive.ForeColor = Color.Black;
+
+            if (!timer_RealTime.Enabled)
+            {
+                btStartMactive.Text = "Start";
+                btStartMactive.BackColor = Color.Transparent;
+            }
+            else
+            {
+                btStartMactive.BackColor = Color.Indigo;
+                btStartMactive.Text = "Stop";
+            }
+            ListBox.Items.Clear();
+
+        }
+
+
+
+        private string GetReturnCodeDesc(int cmdRet)
+        {
+            switch (cmdRet)
+            {
+                case 0x00:
+                case 0x26:
+                    return "success";
+                case 0x01:
+                    return "Return before Inventory finished";
+                case 0x02:
+                    return "the Inventory-scan-time overflow";
+                case 0x03:
+                    return "More Data";
+                case 0x04:
+                    return "Reader module MCU is Full";
+                case 0x05:
+                    return "Access Password Error";
+                case 0x09:
+                    return "Destroy Password Error";
+                case 0x0a:
+                    return "Destroy Password Error Cannot be Zero";
+                case 0x0b:
+                    return "Tag Not Support the command";
+                case 0x0c:
+                    return "Use the commmand,Access Password Cannot be Zero";
+                case 0x0d:
+                    return "Tag is protected,cannot set it again";
+                case 0x0e:
+                    return "Tag is unprotected,no need to reset it";
+                case 0x10:
+                    return "There is some locked bytes,write fail";
+                case 0x11:
+                    return "can not lock it";
+                case 0x12:
+                    return "is locked,cannot lock it again";
+                case 0x13:
+                    return "Parameter Save Fail,Can Use Before Power";
+                case 0x14:
+                    return "Cannot adjust";
+                case 0x15:
+                    return "Return before Inventory finished";
+                case 0x16:
+                    return "Inventory-Scan-Time overflow";
+                case 0x17:
+                    return "More Data";
+                case 0x18:
+                    return "Reader module MCU is full";
+                case 0x19:
+                    return "'Not Support Command Or AccessPassword Cannot be Zero";
+                case 0x1A:
+                    return "Tag custom function error";
+                case 0xF8:
+                    return "Check antenna error";
+                case 0xF9:
+                    return "Command execute error";
+                case 0xFA:
+                    return "Get Tag,Poor Communication,Inoperable";
+                case 0xFB:
+                    return "No Tag Operable";
+                case 0xFC:
+                    return "Tag Return ErrorCode";
+                case 0xFD:
+                    return "Command length wrong";
+                case 0xFE:
+                    return "Illegal command";
+                case 0xFF:
+                    return "Parameter Error";
+                case 0x30:
+                    return "Communication error";
+                case 0x31:
+                    return "CRC checksummat error";
+                case 0x32:
+                    return "Return data length error";
+                case 0x33:
+                    return "Communication busy";
+                case 0x34:
+                    return "Busy,command is being executed";
+                case 0x35:
+                    return "ComPort Opened";
+                case 0x36:
+                    return "ComPort Closed";
+                case 0x37:
+                    return "Invalid Handle";
+                case 0x38:
+                    return "Invalid Port";
+                case 0xEE:
+                    return "Return Command Error";
+                default:
+                    return "";
+            }
+        }
+
+        private void btStartMactive_Click(object sender, EventArgs e)
+        {
+            timer_RealTime.Enabled = !timer_RealTime.Enabled;
+            if (!timer_RealTime.Enabled)
+            {
+                btStartMactive.Text = "Start";
+                btStartMactive.BackColor = Color.Transparent;
+            }
+            else
+            {
+                fInventory_EPC_List = "";
+                ListBox.Items.Clear();
+                btStartMactive.BackColor = Color.Indigo;
+                fIsInventoryScan = false;
+                btStartMactive.Text = "Stop";
+                btStartMactive.ForeColor = Color.Transparent;
+
+
+            }
+        }
+
+        private void timer_RealTime_Tick(object sender, EventArgs e)
+        {
+            if (fIsInventoryScan) return;
+            fIsInventoryScan = true;
+            GetRealtiemeData();
+            fIsInventoryScan = false;
+        }
+
+        private void GetRealtiemeData()
+        {
+            byte[] ScanModeData = new byte[40960];
+            int nLen, NumLen;
+            string temp1 = "";
+            string binarystr1 = "";
+            string binarystr2 = "";
+
+            string EPCStr = "";
+            int ValidDatalength;
+            string temp;
+            ValidDatalength = 0;
+            DataGridViewRow rows = new DataGridViewRow();
+            int xtime = System.Environment.TickCount;
+            fCmdRet = RWDev.ReadActiveModeData(ScanModeData, ref ValidDatalength, frmcomportindex);
+            if (fCmdRet == 0)
+            {
+                try
+                {
+                    byte[] daw = new byte[ValidDatalength];
+                    Array.Copy(ScanModeData, 0, daw, 0, ValidDatalength);
+                    temp = ByteArrayToHexString(daw);
+                    fInventory_EPC_List = fInventory_EPC_List + temp; //把字符串存进列表
+                    nLen = fInventory_EPC_List.Length;
+                    while (fInventory_EPC_List.Length > 18)
+                    {
+                        string FlagStr = Convert.ToString(fComAdr, 16).PadLeft(2, '0') + "EE00"; //查找头位置标志字符串
+                        int nindex = fInventory_EPC_List.IndexOf(FlagStr);
+                        if (nindex > 1)
+                            fInventory_EPC_List = fInventory_EPC_List.Substring(nindex - 2);
+                        else
+                        {
+                            fInventory_EPC_List = fInventory_EPC_List.Substring(2);
+                            continue;
+                        }
+
+                        NumLen = Convert.ToInt32(fInventory_EPC_List.Substring(0, 2), 16) * 2 + 2; //取第一个帧的长度
+                        if (fInventory_EPC_List.Length < NumLen)
+                        {
+                            break;
+                        }
+
+                        temp1 = fInventory_EPC_List.Substring(0, NumLen);
+                        fInventory_EPC_List = fInventory_EPC_List.Substring(NumLen);
+                        if (!CheckCRC(temp1)) continue;
+                        EPCStr = temp1.Substring(12, temp1.Length - 18);
+
+                        ListBox.Items.Add(EPCStr);
+                    }
+                }
+                catch (System.Exception ex)
+                {
+                    ex.ToString();
+                }
+            }
+        }
+
+        public static string ByteArrayToHexString(byte[] data)
+        {
+            StringBuilder sb = new StringBuilder(data.Length * 3);
+            foreach (byte b in data)
+                sb.Append(Convert.ToString(b, 16).PadLeft(2, '0'));
+            return sb.ToString().ToUpper();
+
+        }
+
+        private bool CheckCRC(string s)
+        {
+            int i, j;
+            int current_crc_value;
+            byte crcL, crcH;
+            byte[] data = HexStringToByteArray(s);
+            current_crc_value = 0xFFFF;
+            for (i = 0; i <= (data.Length - 1); i++)
+            {
+                current_crc_value = current_crc_value ^ (data[i]);
+                for (j = 0; j < 8; j++)
+                {
+                    if ((current_crc_value & 0x01) != 0)
+                        current_crc_value = (current_crc_value >> 1) ^ 0x8408;
+                    else
+                        current_crc_value = (current_crc_value >> 1);
+                }
+            }
+            crcL = Convert.ToByte(current_crc_value & 0xFF);
+            crcH = Convert.ToByte((current_crc_value >> 8) & 0xFF);
+            if (crcH == 0 && crcL == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        public static byte[] HexStringToByteArray(string s)
+        {
+            s = s.Replace(" ", "");
+            byte[] buffer = new byte[s.Length / 2];
+            for (int i = 0; i < s.Length; i += 2)
+                buffer[i / 2] = (byte)Convert.ToByte(s.Substring(i, 2), 16);
+            return buffer;
+        }
+    }
+}
+
+
+
